0b512ac22fa89af9856453d8fccfa959fd2e4341
ashadhadi676-cell <ashadhadi676@gmail.com>
2026-01-06 10:42:59 +0530

    Create index.js

---
+++ import React from 'react'; import ReactDOM from 'react-dom'; import DoodleLeap from './DoodleLeap';  ReactDOM.render(   <React.StrictMode>     <DoodleLeap />   </index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DoodleLeap from './DoodleLeap';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DoodleLeap />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
